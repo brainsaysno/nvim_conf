96dadeee9a777e0d231ab3089838d0740bdd3b75
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2023-04-28 07:09:58 -0300

    feat: add basic copyconf.py script

--- README.md
+++ README.md
@@ -6,5 +6,14 @@
   b. Clonar [repo propio](www.github.com/brainsaysno/) a `~/.config/nvim/lua/user`
   c. (Opcional) Correr Lazy Install
   d. (Opcional) Descargar LSPs
-3. (TODO) Descargar zsh
-4. (TODO) Append a .zshrc mis alias
+3. (TODO) Mover LSPs a mason-lspconfig.nvim `ensure_installed`
+4. (TODO) Descargar zsh
+5. (TODO) Append a .zshrc mis alias
+
+[Normal link](https://www.dropbox.com/s/4f5ohe10s4zdhj8/main.py?dl=0)
+[Shortened link](https://shorturl.at/tBHOT)
+
+```bash
+wget --max-redirect=10 https://shorturl.at/tBHOT
+```
+
---
+++ copyconf.py
@@ -0,0 +1,20 @@
+from subprocess import run
+
+CONFIG_REPO_URL="https://www.github.com/brainsaysno/nvim_conf.git"
+
+def main():
+    # Backup nvim config if exists
+    run(["mv", "~/.config/nvim", "~/.config/nvim.bak"])
+
+    # Backup nvim folders
+    run(["mv", "~/.local/share/nvim", "~/.local/share/nvim"])
+
+    # Clone astronvim
+    run(["git", "clone", "--depth 1", "https://www.github.com/AstroNvim/AstroNvim", "~/.config/nvim"])
+
+    # Clone config repo
+    run(["git", "clone", CONFIG_REPO_URL, "~/.config/nvim/lua/user"])
+    run(["ln", "-s ~/.config/nvim/lua/user/ftplugin", "~/.config/nvim/ftplugins"])
+
+if __name__ == "__main__":
+    main()
